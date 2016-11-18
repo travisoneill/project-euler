15cf180980e8cabfba7bb58931d2a8e1464b5953
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-11-18 12:53:59 -0800

    clean up 5

--- 005.py
+++ 005.py
@@ -1,16 +1,12 @@
 from math import sqrt, log
 from functools import reduce
+from collections import defaultdict
+from time import time
 
 #ONE LINER
 print(reduce(lambda a, b: a * b, [n ** int(log(20, n)) for n in (x for x in range(2, 20) if not [y for y in range(2, int(sqrt(x))+1) if x % y == 0])]))
 
-# primes = lambda n: (x for x in range(2, n) if not [y for y in range(2, int(sqrt(x))+1) if x % y == 0])
-# x = reduce(lambda a, b: a * b, [n ** int(log(20, n)) for n in primes(20)])
-# print(x)
-
-
-#OLD APPROACH
-from collections import defaultdict
+#FAST
 def f_count(n):
     count = defaultdict(int, {})
     i = 2
@@ -29,4 +25,19 @@ d = defaultdict(int, {})
 for n in range(2, 21):
     d = max_merge(d, f_count(n))
 
-# print(d)
+#BENCHMARK
+def fast():
+    reduce(lambda x, y: x * y, [k ** v for k, v in d.items()])
+
+def fun():
+    reduce(lambda a, b: a * b, [n ** int(log(20, n)) for n in (x for x in range(2, 20) if not [y for y in range(2, int(sqrt(x))+1) if x % y == 0])])
+
+def bm(f, n):
+    t0 = time()
+    for _ in range(n):
+        f()
+    t1 = time()
+    print(t1-t0)
+
+bm(fun, 10000)
+bm(fast, 10000)
