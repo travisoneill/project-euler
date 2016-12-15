0c386aac83577fc98b2fb3f2cad72b763a198925
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-12-14 20:41:58 -0800

    answer to 39

---
+++ 037.py
@@ -0,0 +1,33 @@
+from euler_utils_primesandfactors import is_prime
+
+def run(n):
+    primes = {}
+    def primetest(n):
+        if n in primes:
+            return primes[n]
+        else:
+            primes[n] = is_prime(n)
+            return primes[n]
+
+    def is_truncatable_prime(n):
+        power = 1
+        while n % 10**power != n:
+            # print(n % 10**power)
+            if not primetest(n % 10**power): return False
+            power += 1
+        while n > 0:
+            # print(n)
+            if not primetest(n): return False
+            n //= 10
+        return True
+
+    count = 0
+    result = 0
+    number = 11
+    while count < 11:
+        if is_truncatable_prime(number):
+            count += 1
+            result += number
+        number += 2
+    print(result)
+    return result
---
+++ 039.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+problem9 = lambda n: { (a, int(sqrt(c**2 - a**2)), c) for a in range(1, 1 + (n * 3) // 10) for c in range((n * 4) // 10, (n * 5) // 10) if sqrt(c**2 - a**2) % 1 == 0 and a + sqrt(c**2 - a**2) + c == n }
+
+mx = 0
+max_triples = 0
+for n in range(0, 1000, 2):
+    p = problem9(n)
+    if len(p) > mx:
+        mx = len(p)
+        max_triples = n
+        # print(p, len(p))
+print(max_triples)
